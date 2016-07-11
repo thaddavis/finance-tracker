87c0d0a486379db82e4ec19ea5c4af1069b5981b
thaddavis <thaddadavis@gmail.com>
2016-07-11 19:22:28 -0400

    Add friendship functionality

---
+++ app/assets/javascripts/friends.js
@@ -0,0 +1,30 @@
+var init_friend_lookup;
+
+init_friend_lookup = function() {
+
+  $('#friend-lookup-form').on('ajax:before', function(event,data,status) {
+    show_spinner();
+  });
+
+  $('#friend-lookup-form').on('ajax:after', function(event,data,status) {
+    hide_spinner();
+  });
+
+  $('#friend-lookup-form').on('ajax:success', function(event,data,status) {
+      debugger;
+      $('#friend-lookup').replaceWith(data);
+      init_friend_lookup();
+  });
+
+  $('#friend-lookup-form').on('ajax:error', function(event,xhr,data,status) {
+      hide_spinner();
+      $('#friend-lookup-results').replaceWith(" ");
+      $('#friend-lookup-errors').replaceWith("friend was not found");
+      init_friend_lookup();
+  });
+
+}
+
+$(document).ready(function() {
+  init_friend_lookup();
+})
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   delete 'delete_my_stock', to: "user_stocks#delete_my_stock", method: :delete
   resources :user_stocks, except: [:show, :edit, :update, :delete]
+  resources :users, only: [:show]
+  resources :friendships
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -13,7 +15,8 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
   get 'my_friends', to: "users#my_friends"
-
+  get 'search_friends', to: "users#search"
+  post 'add_friend', to: "users#add_friend"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
